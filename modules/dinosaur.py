b5e0e126f7a2cb9547e0888c0134fad47e31b642
AS1 <arseniykofanov@gmail.com>
2022-12-28 21:15:45 +0300

    🦕 Написан минимальный класс динозавра.

--- modules/dinosaur.py
+++ modules/dinosaur.py
@@ -1,6 +1,18 @@
+from pprint import pprint
 
+import config
+dinosaurs = config.CLUSTER_CLIENT.bot.dinosaurs
 
 class Dino:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, baseid) -> None:
+        self.id = baseid
+        self.data = dinosaurs.find_one({"_id": self.id})
+    
+    def __str__(self) -> str:
+        return f"DinoObj {self.data['name']}"
+
+
+    def view(self):
+        print('DATA: ', end='')
+        pprint(self.data)
