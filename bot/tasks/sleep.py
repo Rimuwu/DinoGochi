41e3814f3b6f1be97c5d0b7f7723627b70559b96
AS1 <arseniykofanov@gmail.com>
2023-09-04 15:28:21 +0300

    -

--- bot/tasks/sleep.py
+++ bot/tasks/sleep.py
@@ -2,7 +2,7 @@ from time import time
 from random import uniform, randint
 
 from bot.config import conf, mongo_client
-from bot.modules.dinosaur import end_sleep
+from bot.modules.dinosaur import end_sleep, mutate_dino_stat
 from bot.taskmanager import add_task
 from bot.modules.mood import add_mood, check_inspiration
 
@@ -13,38 +13,37 @@ LONG_SLEEP_COLDOWN_MIN = 30
 
 DREAM_CHANCE = 0.01
 
-async def one_time(data, one_time_unit):
-    for sleeper in data:
-        add_energy, sec_time = 0, 0
-        dino = dinosaurs.find_one({'_id': sleeper['dino_id']})
+async def one_time(sleeper, one_time_unit):
+    add_energy, sec_time = 0, 0
+    dino = dinosaurs.find_one({'_id': sleeper['dino_id']})
 
-        if check_inspiration(sleeper['dino_id'], 'sleep'): 
-            one_time_unit *= 2
+    if check_inspiration(sleeper['dino_id'], 'sleep'): 
+        one_time_unit *= 2
 
-        if sleeper['sleep_type'] == 'long':
-            sec_time = int(time()) - sleeper['sleep_start']
-        elif sleeper['sleep_type'] == 'short':
-            sec_time = sleeper['sleep_end'] - sleeper['sleep_start']
+    if sleeper['sleep_type'] == 'long':
+        sec_time = int(time()) - sleeper['sleep_start']
+    elif sleeper['sleep_type'] == 'short':
+        sec_time = sleeper['sleep_end'] - sleeper['sleep_start']
 
-        if dino:
-            energy = dino['stats']['energy']
-            if energy >= 100:
-                await end_sleep(sleeper['dino_id'], sec_time)
-            else:
-                if energy + one_time_unit >= 100:
-                    add_energy = 100 - energy
-                    await end_sleep(sleeper['dino_id'], sec_time)
-                else: add_energy = one_time_unit
-
-                if uniform(0, 1) <= DREAM_CHANCE:
-                    if randint(1, 3) == 2:
-                        add_mood(dino['_id'], 'bad_dream', -1, 2700, True)
-                    else:
-                        add_mood(dino['_id'], 'dream', 1, 2700, True)
-
-                dinosaurs.update_one({'_id': dino['_id']}, {'$inc': {'stats.energy': add_energy}})
+    if dino:
+        energy = dino['stats']['energy']
+        if energy >= 100:
+            await end_sleep(sleeper['dino_id'], sec_time)
         else:
-            sleepers.delete_one({"_id": sleeper['_id']})
+            if energy + one_time_unit >= 100:
+                add_energy = 100 - energy
+                await end_sleep(sleeper['dino_id'], sec_time)
+            else: add_energy = one_time_unit
+
+            if uniform(0, 1) <= DREAM_CHANCE:
+                if randint(1, 3) == 2:
+                    add_mood(dino['_id'], 'bad_dream', -1, 2700, True)
+                else:
+                    add_mood(dino['_id'], 'dream', 1, 2700, True)
+
+            await mutate_dino_stat(dino, 'energy', add_energy)
+    else:
+        sleepers.delete_one({"_id": sleeper['_id']})
 
 async def check_notification():
     """Уведомления и окончание сна
@@ -65,11 +64,15 @@ async def check_notification():
 
 async def short_check():
     data = list(sleepers.find({'sleep_type': 'short'})).copy()
-    await one_time(data, 1)
+    for sleeper in data: await one_time(sleeper, 1)
+
+    print(time(), 'start short_check')
 
 async def long_check():
     data = list(sleepers.find({'sleep_type': 'long'})).copy()
-    await one_time(data, 1)
+    for sleeper in data: await one_time(sleeper, 1)
+
+    print(time(), 'start long_check')
 
 if __name__ != '__main__':
     if conf.active_tasks:
