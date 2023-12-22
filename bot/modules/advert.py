e87099578cc6f87d01f9ff680e47bdd6fcfc61b0
AS1 <arseniykofanov@gmail.com>
2023-12-22 19:41:05 +0300

    Подготовка к рекламе

---
+++ bot/modules/advert.py
@@ -0,0 +1,21 @@
+
+import aiohttp
+from config import conf
+from bot.modules.user import premium
+from bot.modules.logs import log
+
+async def show_advert(user_id: int):
+    if not await premium(user_id):
+        async with aiohttp.ClientSession() as session:
+
+            async with session.post(
+                'https://api.gramads.net/ad/SendPost',
+                headers={
+                    'Authorization': conf.donation_token,
+                    'Content-Type': 'application/json',
+                },
+                json={'SendToChatId': user_id},
+            ) as response:
+                if not response.ok:
+                    log('Gramads: %s' % str(await response.json()), 2)
+
