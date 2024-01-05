c0154a72fe2d13ee5603ab6f798aabb103310120
AS1 <arseniykofanov@gmail.com>
2024-01-06 01:16:56 +0300

    -

--- bot/middlewares/antiflood.py
+++ bot/middlewares/antiflood.py
@@ -41,7 +41,7 @@ class AntifloodMiddleware(BaseMiddleware):
                         now = datetime.now(timezone.utc)
                         delta = now - create
 
-                        if delta.seconds >= 1209600 or True: 
+                        if delta.seconds >= 1209600: 
 
                             if message.from_user.id in self.last_ads.keys():
                                 if int(time_now()) - self.last_ads[message.from_user.id] >= 7_200:
