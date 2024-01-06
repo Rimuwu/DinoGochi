4d944b5b64205c6f01628635ab1fc2afc4725f39
AS1 <arseniykofanov@gmail.com>
2024-01-07 01:12:48 +0300

    Возврат кода статуса, начисление супер коинов и уведомление

--- bot/modules/advert.py
+++ bot/modules/advert.py
@@ -3,8 +3,31 @@ import aiohttp
 from bot.config import conf
 from bot.modules.user import premium
 from bot.modules.logs import log
+from bot.config import mongo_client
+import json
+from bot.exec import bot
+from bot.modules.localization import t, get_lang
+
+users = mongo_client.user.users
 
 async def show_advert(user_id: int):
+    """ Показ рекламы через площадку gramads.net
+    
+    Undefined = 0,
+    Success = 1,
+    RevokedTokenError = 2,
+    UserForbiddenError = 3,
+    ToManyRequestsError = 4,
+    OtherBotApiError = 5,
+    OtherError = 6,
+
+    AdLimited = 7,
+    NoAds = 8,
+    BotIsNotEnabled=9,
+    Banned=10,
+    InReview=11
+    """
+    res = 6
     if not await premium(user_id):
         async with aiohttp.ClientSession() as session:
 
@@ -16,7 +39,17 @@ async def show_advert(user_id: int):
                 },
                 json={'SendToChatId': user_id},
             ) as response:
+                data = json.loads(await response.read())
+                res = data['SendPostResult']
 
                 if not response.ok:
                     log('Gramads: %s' % str(await response.json()), 2)
+    
+    if res == 1:
+        await users.update_one({"userid": user_id}, {'$inc': {"super_coins": 1}})
+
+        lang = await get_lang(user_id)
+        await bot.send_message(user_id, t('super_coins.plus_one', lang), parse_mode="Markdown")
+
+    return res
 
