4cb09ad6481e04476ba34c8161deb15c22487d35
AS1 <arseniykofanov@gmail.com>
2024-01-09 19:07:21 +0300

    Фикс аукциона

--- bot/tasks/market_check.py
+++ bot/tasks/market_check.py
@@ -15,35 +15,39 @@ preferential = mongo_client.market.preferential
 async def market_delete():
     # Удаляет старые продукты
     data = await products.find({'add_time': {'$lte': int(time()) - 86_400 * 31}}
-                                    ).to_list(None)  
+                                    ).to_list(None)   # type: ignore
     for i in data: await delete_product(i['_id'])
 
 async def auction_end():
     # Завершение аукциона
     data = await products.find({'end': {'$lte': int(time())}}
-                              ).to_list(None) 
+                              ).to_list(None)  # type: ignore
 
     for i in data:
-        for user in i['users']:
-            status = await users.find_one({'user_id': user['userid']})
-            if status:
-                await products.update_one({'_id': i['_id']}, {'$set': {
-                    f'users.{i["users"].index(user)}.status': 'win'
-                }})
-                await take_coins(i['owner_id'], i['coins'], True)
-                break
+        users_data = list(i['users'])
+
+        max_coins = 0
+        winner = None
+        for user in users_data:
+            status = await users.find_one({'userid': user['userid']})
+            if status and user['coins'] >= max_coins:
+
+                max_coins = user['coins']
+                winner = i["users"].index(user)
+
+        if winner != None:
+            s = await products.update_one({'_id': i['_id']}, 
+                    {'$set': {f'users.{winner}.status': 'win'}})
+            s.raw_result
 
         await delete_product(i['_id'])
 
 async def preferential_delete():
     # Удаляет продвижение
-    data = list(await products.find({'end': {'$lte': int(time())}}
-                                    ).to_list(None) 
-                ).copy()
-    for i in data: await preferential.delete_one({'_id': i['_id']})
+    await preferential.delete_many({'end': {'$lte': int(time())}})
 
 if __name__ != '__main__':
     if conf.active_tasks:
         add_task(market_delete, 43200, 20.0)
-        add_task(auction_end, 7200, 15.0)
+        add_task(auction_end, 600, 15.0)
         add_task(preferential_delete, 7200, 20.0)
