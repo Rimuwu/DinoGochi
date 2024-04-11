3f80f9b4d3457339d7a84f9501dca7de0a662395
AS1 <arseniykofanov@gmail.com>
2024-04-11 18:43:42 +0300

    -

--- bot/handlers/transition.py
+++ bot/handlers/transition.py
@@ -8,6 +8,7 @@ from telebot.types import Message, CallbackQuery, InlineKeyboardMarkup
 from bot.config import mongo_client
 from bot.const import GAME_SETTINGS as GS
 from bot.exec import bot
+from bot.modules.advert import auto_ads
 from bot.modules.data_format import list_to_inline, seconds_to_str, user_name
 from bot.modules.images import async_open
 from bot.modules.item import counts_items, AddItemToUser
@@ -53,6 +54,8 @@ async def settings_menu(message: Message):
 
         await bot.send_message(message.chat.id, text, 
                                reply_markup= await m(userid, 'settings_menu', lang))
+    
+        await auto_ads(message)
 
 @bot.message_handler(pass_bot=True, text='commands_name.settings.settings_page_2', is_authorized=True)
 async def settings2_menu(message: Message):
@@ -71,6 +74,8 @@ async def settings2_menu(message: Message):
 
         await bot.send_message(message.chat.id, text, 
                                reply_markup= await m(userid, 'settings2_menu', lang))
+        
+        await auto_ads(message)
 
 @bot.message_handler(pass_bot=True, text='commands_name.profile_menu', is_authorized=True)
 async def profile_menu(message: Message):
@@ -79,6 +84,8 @@ async def profile_menu(message: Message):
 
     await bot.send_message(message.chat.id, t('menu_text.profile', lang), 
                            reply_markup= await m(userid, 'profile_menu', lang))
+
+    await auto_ads(message)
     
 @bot.message_handler(pass_bot=True, text='commands_name.friends_menu', is_authorized=True)
 async def friends_menu(message: Message):
@@ -87,6 +94,8 @@ async def friends_menu(message: Message):
 
     await bot.send_message(message.chat.id, t('menu_text.friends', lang), 
                            reply_markup= await m(userid, 'friends_menu', lang))
+
+    await auto_ads(message)
 
 @bot.message_handler(pass_bot=True, text='commands_name.profile.market', is_authorized=True)
 async def market_menu(message: Message):
@@ -116,6 +125,8 @@ async def market_menu(message: Message):
             markup = list_to_inline([rand_p], 1)
             await bot.send_message(message.chat.id, t('menu_text.market.products', lang), 
                                 reply_markup=markup, parse_mode='Markdown')
+    
+    await auto_ads(message)
 
 
 @bot.message_handler(pass_bot=True, text='commands_name.actions_menu', is_authorized=True)
@@ -125,6 +136,8 @@ async def actions_menu(message: Message):
 
     await bot.send_message(message.chat.id, t('menu_text.actions', lang), 
                            reply_markup = await m(userid, 'actions_menu', lang))
+    
+    await auto_ads(message)
 
 @bot.message_handler(pass_bot=True, text='commands_name.dino-tavern_menu', is_authorized=True)
 async def tavern_menu(message: Message):
@@ -187,6 +200,8 @@ async def tavern_menu(message: Message):
                 col = await tavern.count_documents({}))
 
         await bot.edit_message_text(text=text, chat_id=userid, message_id=msg.message_id)
+    
+    await auto_ads(message)
 
 @bot.message_handler(pass_bot=True, text='commands_name.profile.about', is_authorized=True)
 async def about_menu(message: Message):
@@ -218,6 +233,8 @@ async def about_menu(message: Message):
            parse_mode ='HTML'
            )
 
+    await auto_ads(message)
+
 @bot.message_handler(pass_bot=True, text='commands_name.friends.referal', is_authorized=True)
 async def referal_menu(message: Message):
     userid = message.from_user.id
@@ -237,6 +254,8 @@ async def referal_menu(message: Message):
                 award_text=award_text, lvl=lvl), 
                 parse_mode='Markdown',
                 reply_markup= await m(userid, 'referal_menu', lang))
+
+    await auto_ads(message)
 
 @bot.callback_query_handler(pass_bot=True, func=lambda call: call.data.startswith('buy_ale'))
 async def buy_ale(callback: CallbackQuery):
@@ -267,6 +286,8 @@ async def seller_profile(message: Message):
     await bot.send_message(message.chat.id, t('menu_text.seller', lang), 
                            reply_markup= await m(userid, 'seller_menu', lang))
     
+    await auto_ads(message)
+    
 @bot.message_handler(pass_bot=True, text='commands_name.market.background_market', is_authorized=True)
 async def backgrounds(message: Message):
     userid = message.from_user.id
@@ -274,3 +295,5 @@ async def backgrounds(message: Message):
 
     await bot.send_message(message.chat.id, t('menu_text.backgrounds_menu', lang), 
                            reply_markup= await m(userid, 'backgrounds_menu', lang))
+
+    await auto_ads(message)
