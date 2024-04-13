427e3803a8a142470b3cc59a4729b98cad28c4ac
AS1 <arseniykofanov@gmail.com>
2024-04-13 23:12:33 +0300

    Возврат

--- bot/middlewares/antiflood.py
+++ bot/middlewares/antiflood.py
@@ -5,29 +5,33 @@ from telebot.types import Message
 from bot.exec import bot
 from bot.config import mongo_client
 from time import time as time_now
-from bot.modules.advert import auto_ads
 
-DEFAULT_RATE_LIMIT = 0.2
+DEFAULT_RATE_LIMIT = 0.1
 users = mongo_client.user.users
 
 class AntifloodMiddleware(BaseMiddleware):
 
     def __init__(self, limit=DEFAULT_RATE_LIMIT):
-        # self.last_time = {}
-        # self.limit = limit
+        self.last_time = {}
+        self.limit = limit
         self.update_types = ['message']
 
     async def pre_process(self, message: Message, data: dict):
-        # if not message.from_user.id in self.last_time:
-        #     self.last_time[message.from_user.id] = time_now()
-        #     return 
-        # if time_now() - self.last_time[message.from_user.id] < self.limit:
-        #     return SkipHandler()
-        # self.last_time[message.from_user.id] = time_now()
+        if not message.from_user.id in self.last_time:
+            self.last_time[message.from_user.id] = time_now()
+            return 
+        if time_now() - self.last_time[message.from_user.id] < self.limit:
+            return SkipHandler()
+        self.last_time[message.from_user.id] = time_now()
         return
 
     async def post_process(self, message: Message, data, exception):
-        await auto_ads(message)
+        user_id = message.from_user.id
+        if message.chat.type == "private":
+            user = await users.find_one({'userid': user_id}, {"_id": 1})
+            if user:
+                await users.update_one({'userid': user_id}, 
+                                    {'$set': {'last_message_time': message.date}})
 
 
-# bot.setup_middleware(AntifloodMiddleware())
+bot.setup_middleware(AntifloodMiddleware())
