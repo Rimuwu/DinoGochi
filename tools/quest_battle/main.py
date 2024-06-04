18600c0de5ecde5d5ad1577f3b3e52229dac8ad6
AS1 <arseniykofanov@gmail.com>
2024-06-04 13:48:37 +0300

    -

--- tools/quest_battle/main.py
+++ tools/quest_battle/main.py
@@ -7,6 +7,10 @@ back_file = 'back1.png'
 l = ImageFont.truetype('fonts/edosz.ttf', size=18)
 b = ImageFont.truetype('fonts/emoji.ttf', size=100)
 
+import json
+with open('bot/json/dino_data.json', encoding='utf-8') as f: 
+    DINOS = json.load(f) # type: dict
+
 def trans_paste(fg_img: Image.Image, bg_img: Image.Image, 
                 alpha=10.0, box=(0, 0)):
     """Накладывает одно изображение на другое.
@@ -25,18 +29,23 @@ def crop_text(text: str, unit: int=10, postfix: str='...'):
         return text[:unit] + postfix
     else: return text
 
-def generate(data_player_1, data_player_2):
+def generate(data_player_1, data_player_2, game_data):
     """ 
     data_player_1: {
         "name": "as1",
         "points": 12
     }
-    
+
     data_player_2: {
         "name": "cat",
         "points": 56
     }
-    
+
+    game_data: {
+        "stage": 2,
+        "tasks": []
+    }
+
     """
 
     # Фон
@@ -62,12 +71,44 @@ def generate(data_player_1, data_player_2):
     idraw.text((67 + line_len + 25, 29), str(data_player_1['points']), font = l)
     idraw.text((67 + line_len + 25, 62), str(data_player_2['points']), font = l)
 
+    # # Колечки заданий
+    # circle_white = Image.open(images_dir + 'neutral.png')
+    # circle_red = Image.open(images_dir + 'notaken.png')
+    # circle_green = Image.open(images_dir + 'taken.png').resize((45, 45), Image.Resampling.LANCZOS)
+
+    # for i in range(1, 6):
+    #     if i in game_data['wins']:
+    #         # img = trans_paste(circle_green, img, box=(306, 10))
+            
+    #         x, y = 306, 32.5
+    #         r = 22.5
+            
+    #         leftUpPoint = (x-r, y-r)
+    #         rightDownPoint = (x+r, y+r)
+    #         twoPointList = [leftUpPoint, rightDownPoint]
+    #         idraw.ellipse(twoPointList, outline=(255,0,0,255), width=2)
+    
+
+    # Дино
+    for dino_id in [data_player_1['dino_id'], data_player_2['dino_id']]:
+        dino_data = DINOS['elements'][str(dino_id)]
+        dino_image = Image.open(f'images/{dino_data["image"]}')
+        dino_image = dino_image.resize((512, 512), Image.Resampling.LANCZOS)
+
+        x, y, sz = -20, -100, 512
+        img = trans_paste(dino_image, img, box=((y + x, y, sz + y + x, sz + y)))
+
+    
+
 
 
 
     img.show()
 
-data_player_1 = {'name': 'as1', "points": 12}
-data_player_2 = {'name': 'unknown_artist', "points": 56}
+data_player_1 = {'name': 'as1', "points": 12, 'dino_id': 147}
+data_player_2 = {'name': 'unknown_artist', "points": 56, 'dino_id': 264}
+game_data = {"stage": 2, "tasks": ['fishing', 'battle', 'quest', 'communication', 'hunting'],
+             "wins": [1]
+             }
 
-generate(data_player_1, data_player_2)
+generate(data_player_1, data_player_2, game_data)
