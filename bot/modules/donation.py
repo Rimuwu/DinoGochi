546c75a2de0ad72dfa7278e056249c4bb5b6d89a
AS1 <arseniykofanov@gmail.com>
2024-06-16 16:01:36 +0300

    Работа с платежами

--- bot/modules/donation.py
+++ bot/modules/donation.py
@@ -1,25 +1,17 @@
 import json
 import os
-from time import time
-
-import aiohttp
-
-from bot.config import conf, mongo_client
 from bot.exec import bot
-from bot.modules.currency import get_products, get_all_currency
+from telebot.types import LabeledPrice
+
+from bot.const import GAME_SETTINGS
+from bot.modules.item import AddItemToUser
+from bot.modules.localization import get_data, get_lang
 from bot.modules.logs import log
 from bot.modules.notifications import user_notification
-from bot.modules.item import AddItemToUser
 from bot.modules.user import award_premium
-from bot.modules.localization import  get_lang
-
-users = mongo_client.user.users
 
 directory = 'bot/data/donations.json'
-processed_donations = {}
-headers = {
-    'Authorization': 'Bearer {token}'.format(token=conf.donation_token),
-}
+products = GAME_SETTINGS['products']
 
 def save(data):
     """Сохраняет данные в json
@@ -42,14 +34,6 @@ def OpenDonatData() -> dict:
             log(prefix='OpenDonatData', message=f'Error: {error}', lvl=4)
     return processed_donations
 
-async def get_donations() -> list:
-    """ Получает донаты
-    """
-    async with aiohttp.ClientSession() as session:
-        async with session.get('https://www.donationalerts.com/api/v1/alerts/donations', headers=headers) as response:
-            data = dict(await response.json())
-            try: return data['data']
-            except: return []
 
 def save_donation(userid, amount, status, product, 
             issued_reward, time_data, col):
@@ -87,129 +71,37 @@ async def send_donat_notification(userid:int, message_key:str, **kwargs):
     await user_notification(userid, f'donation', lang, add_way=message_key, **kwargs)
 
 async def give_reward(userid:int, product_key:str, col:int):
-    products = get_products()
     product = products[product_key]
 
     if product['type'] == 'subscription':
         await award_premium(userid, product['time'] * col)
 
-    for item_id in product['items']:
+    for item_id in product['items'] * col:
         await AddItemToUser(userid, item_id)
 
     await send_donat_notification(userid, 'reward')
 
-async def check_donations():
-    processed_donations = OpenDonatData()
-    all_donations = await get_donations()
-    products = get_products()
+async def send_inv(user_id: int, product_id: str, col: int, lang: str, cost: int = 0):
+    products = GAME_SETTINGS['products']
+    if product_id != 'non_repayable':
+        product = products[product_id]
+    else:
+        product = {
+            "cost": {f"{col}": {
+                "XTR": cost
+            } }
+        }
 
-    for donat in all_donations:
-        donation_data = {}
-        product_col = "1"
+    product_t_data = get_data(f'support_command.products_bio.{product_id}', lang)
 
-        if str(donat['id']) not in processed_donations.keys():
-            #Донат не был ранее обработан
-            if int(time()) - donat['created_at_ts'] <= 3024000:
-                if donat['username'].isdigit():
-                    userid = int(donat['username'])
-                    user = users.find_one({'userid': userid})
-                    if user:
-                        message_split = donat['message'].split('#$#')
-                        product_key = message_split[0]
-                        if product_key in products:
-                            product = products[product_key]
+    name = product_t_data['name']
+    short = product_t_data['short']
+    photo_url = product_t_data['photo_url']
 
-                            if product['type'] == 'subscription':
-                                if len(message_split) > 1:
-                                    col = message_split[1]
-                                    if col.isdigit():
-                                        if col in product['cost'].keys():
-                                            product_col = col
+    product_label = LabeledPrice(name, product['cost'][str(col)]['XTR'])
 
-                            if donat['currency'] in get_all_currency():
-                                cost = product['cost'][product_col][donat['currency']]
-                                rub_cost = product['cost'][product_col]['RUB']
-
-                                if donat['amount'] >= cost:
-                                    await give_reward(userid, product_key, int(product_col))
-
-                                    donation_data = save_donation(
-                                        donat['username'], 
-                                        donat['amount'], 'done', 
-                                        product_key, True, donat['created_at_ts'],
-                                        product_col
-                                    )
-
-                                elif donat['amount_in_user_currency'] >= rub_cost:
-                                    await give_reward(userid, product_key, int(product_col))
-
-                                    donation_data = save_donation(
-                                        donat['username'], 
-                                        donat['amount'], 'done', 
-                                        product_key, True, donat['created_at_ts'],
-                                        product_col
-                                    )
-
-                                else:
-                                    #Сумма доната меньше, чем стоимость продукта
-                                    error = 'amount_error'
-                                    donation_data = save_donation(
-                                        donat['username'], 
-                                        donat['amount'], error, 
-                                        product_key, False, donat['created_at_ts'],
-                                        product_col
-                                    )
-
-                                    donation_data['difference'] = [
-                                        cost - donat['amount'], 
-                                        rub_cost - donat['amount_in_user_currency']
-                                        ]
-
-                                    await send_donat_notification(userid, error, 
-                                                                difference=donation_data['difference'][0],
-                                                                currency=donat['currency'],
-                                                                col=product_col
-                                                                )
-                            else:
-                                #Валюта доната не найдена в возможных
-                                error = 'currency_key_error'
-                                donation_data = save_donation(donat['username'], 
-                                    donat['amount'], error, 
-                                    product_key, False, donat['created_at_ts'],
-                                    product_col
-                                    )
-                                donation_data['error_key'] = donat['currency']
-                                await send_donat_notification(userid, error, 
-                                                            currency=donat['currency'])
-                        else:
-                            #неправильно указан ключ товара
-                            error = 'product_key_error'
-                            donation_data = save_donation(donat['username'], 
-                                donat['amount'], error, None, False, donat['created_at_ts'],
-                                product_col
-                                )
-                            donation_data['error_key'] = product_key[:12]
-                            await send_donat_notification(userid, error, 
-                                                        product_key=product_key[:12])
-
-                    else:
-                        #id юзера не найдено в базе
-                        error = 'userid_not_in_base'
-                        donation_data = save_donation(donat['username'], 
-                            donat['amount'], error, None, False, donat['created_at_ts'],
-                            product_col
-                            )
-                else:
-                    #id юзера может быть только число
-                    error = 'userid_error'
-                    donation_data = save_donation(donat['username'], 
-                            donat['amount'], error, None, False, donat['created_at_ts'],
-                            product_col
-                            )
-            processed_donations[str(donat['id'])] = donation_data
-
-        else:
-            if int(time()) - donat['created_at_ts'] >= 3024001:
-                del processed_donations[str(donat['id'])]
-
-    save(processed_donations)
+    await bot.send_invoice(
+        user_id, name, short + f' (x{col})', f"{product_id}#{col}", '', 'XTR', [product_label],
+        photo_url=photo_url, photo_size=512, photo_height=360, photo_width=720, 
+        
+    )
