b924b362a9676033b84e808fdf6f73a026309a70
AS1 <arseniykofanov@gmail.com>
2024-07-30 19:02:27 +0300

    Разговор

---
+++ bot/handlers/actions_speed/talk.py
@@ -0,0 +1,44 @@
+from random import choice, randint, uniform
+
+from bot.config import mongo_client
+from bot.exec import bot
+from bot.modules.dinosaur.kd_activity import save_kd
+from bot.modules.dinosaur.skills import add_skill_point
+from bot.modules.data_format import user_name
+from bot.modules.decorators import HDMessage
+from bot.modules.dinosaur.dinosaur import  set_status
+from bot.modules.localization import get_data, t
+from bot.modules.markup import markups_menu as m
+from bot.modules.dinosaur.mood import add_mood, repeat_activity
+from bot.modules.overwriting.DataCalsses import DBconstructor
+from bot.modules.user.user import User
+from telebot.types import Message
+
+dinosaurs = DBconstructor(mongo_client.dinosaur.dinosaurs)
+long_activity = DBconstructor(mongo_client.dino_activity.long_activity)
+dino_mood = DBconstructor(mongo_client.dinosaur.dino_mood)
+
+@bot.message_handler(textstart='commands_name.speed_actions.pet', dino_pass=True, nothing_state=True, kd_check='talk')
+@HDMessage
+async def talk(message: Message):
+    userid = message.from_user.id
+    user = await User().create(userid)
+    lang = await user.lang
+    last_dino = await user.get_last_dino()
+    chatid = message.chat.id
+
+    percent, _ = await last_dino.memory_percent('action', 'talk', True)
+    await repeat_activity(last_dino._id, percent)
+
+    if randint(1, 10) > 6: status = 'negative' # negative 40%
+    else: status = 'positive' # positive #60%
+
+    await add_mood(last_dino._id, f'{status}_talk', 1, 600)
+    await save_kd(last_dino._id, 'talk', 900)
+
+    text_l: list = get_data(f'talk.themes.{status}', lang)
+    theme = choice(text_l)
+
+    text = t(f'talk.{status}', lang, theme=theme)
+    await bot.send_message(chatid, text,  parse_mode='Markdown',
+        reply_markup=await m(userid, 'speed_actions_menu', lang, True))
