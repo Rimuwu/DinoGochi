edf91ffc2937e91866049a9e05bb66f68c338d1d
AS1 <arseniykofanov@gmail.com>
2024-08-02 20:50:14 +0300

    Перезагрузка

--- bot/const.py
+++ bot/const.py
@@ -1,24 +1,33 @@
 # Модуль констант
-
 import json
 
-with open('bot/json/items.json', encoding='utf-8') as f: 
-    ITEMS = json.load(f) # type: dict
 
-with open('bot/json/dino_data.json', encoding='utf-8') as f: 
-    DINOS = json.load(f) # type: dict
+def load_const():
+    with open('bot/json/items.json', encoding='utf-8') as f: 
+        ITEMS = json.load(f) # type: dict
 
-with open('bot/json/mobs.json', encoding='utf-8') as f: 
-    MOBS = json.load(f) # type: dict
+    with open('bot/json/dino_data.json', encoding='utf-8') as f: 
+        DINOS = json.load(f) # type: dict
 
-with open('bot/json/floors_dungeon.json', encoding='utf-8') as f: 
-    FLOORS = json.load(f) # type: dict
+    with open('bot/json/mobs.json', encoding='utf-8') as f: 
+        MOBS = json.load(f) # type: dict
 
-with open('bot/json/quests_data.json', encoding='utf-8') as f: 
-    QUESTS = json.load(f) # type: list
+    with open('bot/json/floors_dungeon.json', encoding='utf-8') as f: 
+        FLOORS = json.load(f) # type: dict
 
-with open('bot/json/settings.json', encoding='utf-8') as f: 
-    GAME_SETTINGS = json.load(f) # type: dict
+    with open('bot/json/quests_data.json', encoding='utf-8') as f: 
+        QUESTS = json.load(f) # type: list
 
-with open('bot/json/backgrounds.json', encoding='utf-8') as f: 
-    BACKGROUNDS = json.load(f) # type: dict
+    with open('bot/json/settings.json', encoding='utf-8') as f: 
+        GAME_SETTINGS = json.load(f) # type: dict
+
+    with open('bot/json/backgrounds.json', encoding='utf-8') as f: 
+        BACKGROUNDS = json.load(f) # type: dict
+
+    return ITEMS, DINOS, MOBS, FLOORS, QUESTS, GAME_SETTINGS, BACKGROUNDS
+
+ITEMS, DINOS, MOBS, FLOORS, QUESTS, GAME_SETTINGS, BACKGROUNDS = load_const()
+
+def reload_const():
+    global ITEMS, DINOS, MOBS, FLOORS, QUESTS, GAME_SETTINGS, BACKGROUNDS
+    ITEMS, DINOS, MOBS, FLOORS, QUESTS, GAME_SETTINGS, BACKGROUNDS = load_const()
