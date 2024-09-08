73b40b2f223600da336d2ce9791bb9589d45e2c1
AS1 <arseniykofanov@gmail.com>
2024-09-08 20:01:17 +0300

    Убрана реклама из переходов

--- bot/handlers/transition.py
+++ bot/handlers/transition.py
@@ -6,7 +6,6 @@ from time import time
 from bot.config import mongo_client
 from bot.const import GAME_SETTINGS as GS
 from bot.exec import bot
-from bot.modules.user.advert import auto_ads
 from bot.modules.data_format import list_to_inline, seconds_to_str, user_name
 from bot.modules.decorators import HDCallback, HDMessage
 from bot.modules.images import async_open
@@ -56,8 +55,6 @@ async def settings_menu(message: Message):
 
         await bot.send_message(message.chat.id, text, 
                                reply_markup= await m(userid, 'settings_menu', lang))
-    
-        await auto_ads(message)
 
 @bot.message_handler(pass_bot=True, text='commands_name.settings.settings_page_2', is_authorized=True, private=True)
 @HDMessage
@@ -77,8 +74,6 @@ async def settings2_menu(message: Message):
 
         await bot.send_message(message.chat.id, text, 
                                reply_markup= await m(userid, 'settings2_menu', lang))
-        
-        await auto_ads(message)
 
 @bot.message_handler(pass_bot=True, text='commands_name.profile_menu', is_authorized=True, private=True)
 @HDMessage
@@ -89,8 +84,6 @@ async def profile_menu(message: Message):
     await bot.send_message(message.chat.id, t('menu_text.profile', lang), 
                            reply_markup= await m(userid, 'profile_menu', lang))
 
-    await auto_ads(message)
-    
 @bot.message_handler(pass_bot=True, text='commands_name.friends_menu', is_authorized=True, private=True)
 @HDMessage
 async def friends_menu(message: Message):
@@ -99,8 +92,6 @@ async def friends_menu(message: Message):
 
     await bot.send_message(message.chat.id, t('menu_text.friends', lang), 
                            reply_markup= await m(userid, 'friends_menu', lang))
-
-    await auto_ads(message)
 
 @bot.message_handler(pass_bot=True, text='commands_name.profile.market', is_authorized=True, private=True)
 @HDMessage
@@ -133,9 +124,6 @@ async def market_menu(message: Message):
             markup = list_to_inline([rand_p], 1)
             await bot.send_message(message.chat.id, t('menu_text.market.products', lang), 
                                 reply_markup=markup, parse_mode='Markdown')
-    
-    await auto_ads(message)
-
 
 @bot.message_handler(pass_bot=True, text='commands_name.actions_menu', is_authorized=True, private=True)
 @HDMessage
@@ -145,8 +133,6 @@ async def actions_menu(message: Message):
 
     await bot.send_message(message.chat.id, t('menu_text.actions', lang), 
                            reply_markup = await m(userid, 'actions_menu', lang))
-    
-    await auto_ads(message)
 
 @bot.message_handler(pass_bot=True, text='commands_name.dino-tavern_menu', is_authorized=True, private=True)
 @HDMessage
@@ -211,8 +197,6 @@ async def tavern_menu(message: Message):
                 col = await tavern.count_documents({}), comment='tavern_menu')
 
         await bot.edit_message_text(text=text, chat_id=userid, message_id=msg.message_id)
-    
-    await auto_ads(message)
 
 @bot.message_handler(pass_bot=True, text='commands_name.profile.about', is_authorized=True, private=True)
 @HDMessage
@@ -245,8 +229,6 @@ async def about_menu(message: Message):
            parse_mode ='HTML'
            )
 
-    await auto_ads(message)
-
 @bot.message_handler(pass_bot=True, text='commands_name.friends.referal', is_authorized=True, private=True)
 @HDMessage
 async def referal_menu(message: Message):
@@ -267,8 +249,6 @@ async def referal_menu(message: Message):
                 award_text=award_text, lvl=lvl), 
                 parse_mode='Markdown',
                 reply_markup= await m(userid, 'referal_menu', lang))
-
-    await auto_ads(message)
 
 @bot.callback_query_handler(pass_bot=True, func=lambda call: call.data.startswith('buy_ale'))
 @HDCallback
@@ -300,9 +280,7 @@ async def seller_profile(message: Message):
 
     await bot.send_message(message.chat.id, t('menu_text.seller', lang), 
                            reply_markup= await m(userid, 'seller_menu', lang))
-    
-    await auto_ads(message)
-    
+
 @bot.message_handler(pass_bot=True, text='commands_name.market.background_market', is_authorized=True, private=True)
 @HDMessage
 async def backgrounds(message: Message):
@@ -311,8 +289,6 @@ async def backgrounds(message: Message):
 
     await bot.send_message(message.chat.id, t('menu_text.backgrounds_menu', lang), 
                            reply_markup= await m(userid, 'backgrounds_menu', lang))
-
-    await auto_ads(message)
 
 
 @bot.message_handler(pass_bot=True, text='commands_name.action_ask.live_actions', is_authorized=True, private=True)
@@ -324,8 +300,6 @@ async def live_actions(message: Message):
     await bot.send_message(message.chat.id, t('menu_text.live_actions', lang), 
                            reply_markup= await m(userid, 'live_actions_menu', lang))
 
-    await auto_ads(message)
-
 @bot.message_handler(pass_bot=True, text='commands_name.action_ask.extraction', is_authorized=True, private=True)
 @HDMessage
 async def extraction(message: Message):
@@ -334,8 +308,6 @@ async def extraction(message: Message):
 
     await bot.send_message(message.chat.id, t('menu_text.extraction', lang), 
             reply_markup= await m(userid, 'extraction_actions_menu', lang))
-
-    await auto_ads(message)
 
 @bot.message_handler(pass_bot=True, text='commands_name.action_ask.skills_actions', is_authorized=True, private=True)
 @HDMessage
@@ -346,8 +318,6 @@ async def skills_actions(message: Message):
     await bot.send_message(message.chat.id, t('menu_text.skills_actions', lang), 
             reply_markup= await m(userid, 'skills_actions_menu', lang))
 
-    await auto_ads(message)
-
 @bot.message_handler(pass_bot=True, text='commands_name.action_ask.speed_actions', is_authorized=True, private=True)
 @HDMessage
 async def speed_actions(message: Message):
@@ -356,5 +326,3 @@ async def speed_actions(message: Message):
 
     await bot.send_message(message.chat.id, t('menu_text.speed_actions', lang), 
             reply_markup= await m(userid, 'speed_actions_menu', lang))
-
-    await auto_ads(message)
