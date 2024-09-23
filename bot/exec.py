9678a8aa2e2b0a23982e25b788ac619ed85098ed
AS1 <arseniykofanov@gmail.com>
2024-09-23 20:14:34 +0300

    Возврат к стандарту

--- bot/exec.py
+++ bot/exec.py
@@ -37,6 +37,6 @@ def run():
     check()
     # Запуск тасков и бота
     add_task(notify_devs_start) # Уведомление запуска для разрабов
-    add_task(bot.infinity_polling, skip_pending=True, timeout=5, request_timeout=20)
+    add_task(bot.infinity_polling, skip_pending=True, timeout=5)
     log('Все готово! Взлетаем!', prefix='Start')
     run_taskmanager()
