5d057b55afaa8144569068ef112031d1d11cd190
Daniil <magecode7@yandex.ru>
2024-09-28 17:13:38 +0300

    Добавление отчетов в чат админов

--- bot/modules/logs.py
+++ bot/modules/logs.py
@@ -6,13 +6,18 @@ import telebot
 
 from bot.config import conf
 
+last_errors = []
+MAX_ERRORS = 5
+errors_counter = 0
+last_errors_counter = 0
+
 # Logger
 logger = logging.getLogger()
 
 # File logger
 log_filehandler = RotatingFileHandler(
         filename=f"{conf.logs_dir}/{strftime('%Y-%m-%d_%H.%M.%S')}.log", 
-        encoding='utf-8', mode='a+', backupCount=100, maxBytes=1024*1024*10)
+        encoding='utf-8', mode='a+', backupCount=10, maxBytes=1024*1024*10)
 log_streamhandler = logging.StreamHandler()
 log_formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s", datefmt="%F %T")
 log_filehandler.setFormatter(log_formatter)
@@ -36,7 +41,7 @@ queue_listener.start()
 def log(message: str, lvl: int = 1, prefix: str = 'Бот') -> None:
     """
     LVL: \n
-    -1 - Base Log
+    -1 - Base Log\n
     0 - debug (активируется в config)\n
     1 - info\n
     2 - warning\n
@@ -56,5 +61,32 @@ def log(message: str, lvl: int = 1, prefix: str = 'Бот') -> None:
         logger.warning(f"{prefix}: {message}")
     elif lvl == 3:
         logger.error(f"{prefix}: {message}")
+        report_last_error(f"{prefix}: {message}")
     else:
         logger.critical(f"{prefix}: {message}")
+        report_last_error(f"{prefix}: {message}")
+
+# Храним последние ошибки
+def report_last_error(message: str):
+    global errors_counter
+    errors_counter += 1
+    if len(last_errors) >= MAX_ERRORS:
+        last_errors.pop(0)
+
+    last_errors.append(message)
+
+def get_errors_last_count() -> int:
+    global last_errors_counter
+    errors_dif = errors_counter - last_errors_counter
+    last_errors_counter = errors_counter
+    return errors_dif
+
+def get_errors_count() -> int:
+    return errors_counter
+
+
+# Получить последние ошибки
+def get_last_errors() -> list[str]:
+    last_errors_copy = last_errors.copy()
+    last_errors.clear()
+    return last_errors_copy
---
+++ bot/tasks/bot_report.py
@@ -0,0 +1,58 @@
+import asyncio
+import io
+import os
+
+import aiofiles
+import telebot.types
+
+from bot.config import conf
+from bot.exec import bot
+from bot.modules.logs import (MAX_ERRORS, get_errors_count,
+                              get_errors_last_count, get_last_errors, log)
+from bot.taskmanager import add_task
+
+REPEAT_MINUTES = 1
+DELAY = 10
+
+# Создание отчета из последних ошибок и файла логов
+async def create_report():
+    # Получаем последние ошибки
+    errors = get_last_errors()
+
+    # Формируем текст отчета по ошибкам
+    errors_text = ''
+    if errors:
+        for i in range(len(errors)): errors_text += f"{i+1}) `{errors[i]}`\n"
+    else: errors_text = 'Ошибок нет, так держать!'
+    errors_report_text = f'Отчет по работе бота за `{REPEAT_MINUTES}м`:\nВсего ошибок `{get_errors_count()}`\nОшибок с последнего отчета: `{get_errors_last_count()}`\nПоследние `{MAX_ERRORS}` ошибок:\n\n{errors_text}'
+
+    # Получаем список файлов логов
+    logs = os.listdir(conf.logs_dir)
+    if not logs: 
+        log("Файлы логов не найдены", 2)
+    # Массив задач для асинхронной отправки сообщений
+    tasks = []
+
+    for dev in conf.bot_devs:
+        tasks.append(bot.send_message(dev, f'{errors_report_text}', parse_mode='Markdown'))
+
+    if logs:
+        try:
+            # Открываем последний лог файл
+            async with aiofiles.open(f'{conf.logs_dir}/{logs[-1]}', mode='rb') as f:
+                last_log_content = await f.read()  # Читаем содержимое файла
+                last_log = telebot.types.InputFile(io.BytesIO(last_log_content), file_name=logs[-1])
+
+                # Формируем задачи для отправки логов
+                for dev in conf.bot_devs:
+                    tasks.append(bot.send_document(dev, last_log))
+
+        except Exception as e:
+            log(f"Ошибка при чтении файла лога: {e}", 3)
+
+    await asyncio.gather(*tasks)
+
+
+if __name__ != '__main__':
+    if conf.active_tasks:
+        add_task(create_report, REPEAT_MINUTES * 60, DELAY)
