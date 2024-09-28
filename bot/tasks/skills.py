2d9da04cae77537641c1b6c3a2ee55c5d30ca6ed
AS1 <arseniykofanov@gmail.com>
2024-09-28 15:01:57 +0300

    Отображение сколько было повышено хар. Комментарии работы, понижение хар, которая должна быть понижена. Новый способ определения конца тренировки, более лёгкий рандом.

--- bot/tasks/skills.py
+++ bot/tasks/skills.py
@@ -1,5 +1,5 @@
 
-from random import choice, randint, uniform
+from random import choice, randint, random, uniform
 from time import time
 
 from bot.config import conf
@@ -16,6 +16,7 @@ from bot.modules.localization import get_lang, t
 from bot.modules.notifications import dino_notification
 from bot.modules.user.user import get_inventory_from_i
 from bot.taskmanager import add_task
+from bot.modules.logs import log
 
 from bot.modules.overwriting.DataCalsses import DBconstructor
 dinosaurs = DBconstructor(mongo_client.dinosaur.dinosaurs)
@@ -30,7 +31,8 @@ async def end_tranning(skill_activ, dino_id):
 
     await end_skill_activity(dino_id)
     await dino_notification(dino_id,
-                            skill_activ['activity_type'] + '_end_negative')
+                            skill_activ['activity_type'] + '_end_negative', 
+                            add_unit=round(unit_percent, 4))
 
 async def skills_work():
     res_list = await long_activity.find(
@@ -47,25 +49,38 @@ async def skills_work():
         save = True
 
         if dino:
+            # Если дино в тренировке
             up_unit = uniform(*skill_activ['up_unit'])
             down_unit = uniform(*skill_activ['down_unit'])
 
+            # Проверяем, есть ли вдохновение
             insp = await check_inspiration(dino_id, skill_activ['activity_type'])
 
             if insp:
+                # Если есть вдохновение, то умножаем
                 up_unit *= 2
                 down_unit *= 2
 
+            # Добавляем / уменьшаем скилы
             await add_skill_point(dino_id, skill_activ['up_skill'], up_unit)
-            await add_skill_point(dino_id, skill_activ['down_skill'], down_unit)
+            await add_skill_point(dino_id, skill_activ['down_skill'], -down_unit)
 
+            # Проверяем, не пришло ли время
             traning_time = int(time()) - skill_activ['start_time']
-            if traning_time >= skill_activ['max_time']:
-                dif_percent = (traning_time - skill_activ['max_time']) // (skill_activ['max_time'] // 100)
 
-                if randint(1, 5) == 4:
+            dif_percent = (traning_time - skill_activ['max_time']) // (skill_activ['max_time'] // 100)
+
+            if dif_percent > 0:
+                # расчет штрафа за превышение времени
+                # расчетаем сколько процентов времени мы превысили
+                # и делим на 100, чтобы получить процент
+                # например: 7200 - 5400 = 1800 // 540
+
+                if random() <= 0.2:
+                    # 1/5 шанс, что дино будет
+                    # наказано за превышение времени
                     if dif_percent >= 20 and skill_activ['ahtung_lvl'] == 0:
-                        # Повышение кд на 5 часов
+                        # ...повышается кд на 5 часов
                         ahtung_lvl = 1
                         await save_kd(dino_id, skill_activ['activity_type'], 3600 * 5)
 
@@ -76,24 +91,30 @@ async def skills_work():
                         except: pass
 
                     elif dif_percent >= 40 and skill_activ['ahtung_lvl'] == 1:
-                        # Завершение
+                        # ...иначе тренировка заканчивается
                         save = False
                         await end_tranning(skill_activ, dino_id)
 
-                elif randint(1, 10) == 5:
+                elif random() <= 0.1:
+                    # 1/10 шанс, что дино будет
+                    # наказано за превышение времени
                     await dino.update(
                         {'$inc': {
                             'stats.heal': -1
                         }}
                     )
 
-                elif randint(1, 10) == 3:
+                elif random() <= 0.1:
+                    # 1/10 шанс, что дино будет
+                    # наказано за превышение времени
                     await add_mood(dino._id, 'overloading', -1, 3600, True)
 
             if dino.stats['energy'] <= 30 or dino.stats['eat'] <= 15 and save:
+                # Если дино голодно или слабо, то...
+                send_status = False
 
-                if skill_activ['use_energy']:
-                    # Можно использовать энергетики
+                if skill_activ['use_energy'] and dino.stats['energy'] <= 30:
+                    # ...можно использовать энергетики
                     energy_eat = get_group('energy_eat')
                     data_for_f = list(map(
                         lambda i: {'item_id': i}, energy_eat))
@@ -111,11 +132,12 @@ async def skills_work():
                                 await bot.send_message(sended, return_text, parse_mode='Markdown')
                             except: pass
 
-                if dino.stats['energy'] <= 30 or dino.stats['eat'] <= 15:
-                    # Завершаем тренировку 
+                if not send_status and (dino.stats['energy'] <= 30 or dino.stats['eat'] <= 15):
+                    # ...иначе тренировка заканчивается
                     await end_tranning(skill_activ, dino_id)
 
             if save:
+                # Если тренировка не закончилась, то...
                 await long_activity.update_one({'_id': skill_activ['_id']}, {
                     '$set': {
                         'last_check': int(time()),
@@ -126,7 +148,6 @@ async def skills_work():
                     }
                 })
 
-
 if __name__ != '__main__':
     if conf.active_tasks:
-        add_task(skills_work, 300.0, 1.0)
+        add_task(skills_work, 300.0, 10.0)
