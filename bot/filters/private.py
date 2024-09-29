11ada6a14c35a0dae5f7abd7ca4bd34e4e43fab4
AS1 <arseniykofanov@gmail.com>
2024-09-29 18:36:23 +0300

    Корректировка

--- bot/filters/private.py
+++ bot/filters/private.py
@@ -4,21 +4,19 @@ from aiogram.filters import BaseFilter
 from aiogram.types import CallbackQuery, Message
 
 class IsPrivateChat(BaseFilter):
-    def __init__(self, var: Union[CallbackQuery, Message], 
-                 status: str) -> None:
-        self.var: Union[CallbackQuery, Message] = var
-        self.status: str = status
+    def __init__(self, status: bool) -> None:
+        self.status: bool = status
 
-    async def __call__(self, message: Message) -> bool:
+    async def __call__(self, var: Union[CallbackQuery, Message]) -> bool:
         is_private = False
 
-        if type(self.var) == CallbackQuery:
-            if self.var.message:
-                is_private = self.var.message.chat.type == 'private'
+        if type(var) == CallbackQuery:
+            if var.message:
+                is_private = var.message.chat.type == 'private'
 
-        elif type(self.var) == Message:
-            if self.var:
-                is_private = self.var.chat.type == 'private'
+        elif type(var) == Message:
+            if var:
+                is_private = var.chat.type == 'private'
 
         if self.status: result = is_private
         else: result = not is_private
--- bot/filters/states.py
+++ bot/filters/states.py
@@ -4,18 +4,16 @@ from aiogram.filters import BaseFilter
 from aiogram.types import CallbackQuery, Message
 
 class NothingState(BaseFilter):
-    def __init__(self, var: Union[CallbackQuery, Message], 
-                 status: bool) -> None:
-        self.var: Union[CallbackQuery, Message] = var
+    def __init__(self, status: bool) -> None:
         self.status: bool = status
 
-    async def __call__(self):
-        if type(self.var) == CallbackQuery:
-            state = self.var.from_user.__getstate__
+    async def __call__(self, var: Union[CallbackQuery, Message]):
+        if type(var) == CallbackQuery:
+            state = var.from_user.__getstate__
 
-        if type(self.var) == Message:
-            if self.var.from_user:
-                state = self.var.from_user.__getstate__
+        if type(var) == Message:
+            if var.from_user:
+                state = var.from_user.__getstate__
             else: return False
 
         if not state and self.status: return True
