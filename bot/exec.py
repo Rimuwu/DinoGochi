4bd13c003da25ddf1263aa33dbd3056bfc1e4b75
AS1 <arseniykofanov@gmail.com>
2024-09-29 17:50:09 +0300

    Выведен объект Dispatcher для импорта, подключён хендлер ошибок

--- bot/exec.py
+++ bot/exec.py
@@ -1,8 +1,10 @@
 # Исполнитель бота
 import traceback
 
-from bot.dbmanager import check
-from aiogram import Bot, Dispatcher
+from bot.dbmanager import check, mongo_client
+from aiogram import Bot, Dispatcher 
+from aiogram.types import ErrorEvent
+from aiogram.fsm.storage.mongo import MongoStorage
 
 from bot.config import conf
 from bot.modules.logs import log
@@ -10,14 +12,12 @@ from bot.taskmanager import add_task
 from bot.taskmanager import run as run_taskmanager
 import asyncio
 
+bot = Dispatcher(storage=MongoStorage(mongo_client))
+bot_object = Bot(conf.bot_token)
 
-# class TracebackHandler(exception_handler):
-#     def handle(self, exception):
-#         log(f'{traceback.format_exc()} {exception}', 3)
-
-bot = Bot(conf.bot_token)
-# bot = Bot(conf.bot_token, exception_handler=TracebackHandler())
-# bot.enable_saving_states()
+@bot.error()
+async def error_handler(exception: ErrorEvent):
+    log(f'{traceback.format_exc()} {exception}', 3)
 
 async def notify_devs_start():
     # id рассылки
@@ -27,9 +27,9 @@ async def notify_devs_start():
     for id in report_ids:
         if isinstance(id, str):
             channel_id, topic_id = id.split('_', 2)
-            tasks.append(bot.send_message(channel_id, '✅ Бот запущен!', message_thread_id=int(topic_id)))
+            tasks.append(bot_object.send_message(channel_id, '✅ Бот запущен!', message_thread_id=int(topic_id)))
         else: 
-            tasks.append(bot.send_message(id, '✅ Бот запущен!'))
+            tasks.append(bot_object.send_message(id, '✅ Бот запущен!'))
     await asyncio.gather(*tasks)
 
 def run():
@@ -44,8 +44,10 @@ def run():
     log('Ой, да что там ваш ChatGPT?! *Stable Diffusion подключился*')
     # Проверка готовности
     check()
+
     # Запуск тасков и бота
     add_task(notify_devs_start) # Уведомление запуска для разрабов
-    add_task(Dispatcher().start_polling(bot))
+    add_task(bot.start_polling(bot_object))
+
     log('Все готово! Взлетаем!', prefix='Start')
     run_taskmanager()
