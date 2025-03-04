2e6597621f0fff21d49cab44744c50b1d6a4784d
AS1 <arseniykofanov@gmail.com>
2025-03-04 18:04:00 +0300

    -

--- bot/tasks/bot_report.py
+++ bot/tasks/bot_report.py
@@ -40,13 +40,12 @@ async def report_file(file_path: str, file_name: str):
             # Открываем последний лог файл
         async with aiofiles.open(f'{file_path}/{file_name}', mode='rb') as f:
             last_log_content = await f.read()  # Читаем содержимое файла
-            for id in report_ids:
-                last_log = aiogram.types.InputFile(io.BytesIO(last_log_content), file_name=file_name)
-                if isinstance(id, str):
-                    channel_id, topic_id = id.split('_', 2)
-                    tasks.append(bot.send_document(channel_id, last_log, message_thread_id=int(topic_id)))
-                else: 
-                    tasks.append(bot.send_document(id, last_log))
+            last_log = aiogram.types.InputFile(io.BytesIO(last_log_content), filename=file_name)
+            if isinstance(report_id, str):
+                channel_id, topic_id = report_id.split('_', 2)
+                tasks.append(bot.send_document(channel_id, last_log, message_thread_id=int(topic_id)))
+            else: 
+                tasks.append(bot.send_document(report_id, last_log))
 
     except Exception as e:
         log(f"Ошибка при чтении файла лога: {e}", 3)
