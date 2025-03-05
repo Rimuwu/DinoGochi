ca446fcc77c78f069f1327ba15ee423b06eec32e
AS1 <arseniykofanov@gmail.com>
2025-03-06 00:34:46 +0300

    Реализованы потоки и чуть изменена работа с базой

--- bot/minigames/minigame.py
+++ bot/minigames/minigame.py
@@ -1,3 +1,6 @@
+import asyncio
+
+from bson import ObjectId
 from bot.exec import bot
 import random
 from bot.minigames.minigame_registartor import Registry
@@ -6,6 +9,7 @@ from aiogram import types
 from bot.modules.logs import log
 from bot.dbmanager import mongo_client
 from bot.modules.overwriting.DataCalsses import DBconstructor
+import time
 
 database = DBconstructor(mongo_client.minigames.online)
 
@@ -15,30 +19,35 @@ class MiniGame:
     """ Когда обхект класса создан """
 
     def __init__(self):
-        self.GAME_ID = 'BASEMINIGAME' # Этот пункт обязательно должен быть изменён
+        self.GAME_ID: str = 'BASEMINIGAME' # Этот пункт обязательно должен быть изменён
+        self.active_threads: bool = True # Служит для паузы тасков
+        self.__threads_work: bool = True # Служит для отключения цикла
+        self.thread_tick: int = 5
 
         # ======== SESSION ======== #
-        self._id = 0
-        self.session_key = '0000'
-        self.chat_id = 0
-        self.user_id = 0
+        self._id: int = 0
+        self.session_key: str = '0000'
+        self.chat_id: int = 0
+        self.user_id: int = 0
 
         # Дополнительные сообщения в формате {'function_key': message_id}
         # function_key - просто ключ для чего юзается
-        self.session_masseges = {
+        self.session_masseges: dict = {
             'main': 0
         }
 
         # ======== BUTTONS ======== #
-        self.ButtonsRegister = {
+        self.ButtonsRegister: dict = {
             "bn1": "button"
         }
 
-        # ======== TRADES ======== #
-        self.TradesRegister = {
-            "timer": {"repeat": 15, "col_repeat": 1, "function": 'timer',
+        # ======== THREADS ======== #
+        # Желательно, чтобы repeat было кратно 5
+
+        self.ThreadsRegister: dict = {
+            "timer": {"repeat": 5, "col_repeat": 1, "function": 'timer',
                       "last_start": 0
-                      }  # col_repeat - кол-во повторений (или 0)
+                      }  # col_repeat - кол-во повторений (или 'inf')
         }
 
         self.initialization()
@@ -50,8 +59,37 @@ class MiniGame:
 
     def __str__(self):
         return f'{self.GAME_ID}'
-    
-    # ======== TRADES ======== #
+
+    # ======== THREADS ======== #
+
+    async def run_threads(self):
+        """ Запуск вечного цикла для проверки и выполнения потоков """
+        while self.__threads_work:
+            await asyncio.sleep(self.thread_tick)
+
+            if self.active_threads:
+                for key, thread in self.ThreadsRegister.items():
+                    now = time.time()
+
+                    if (thread['last_start'] + thread['repeat'] < now) and (thread['col_repeat'] == 'inf' or thread['col_repeat'] > 0):
+                        function = getattr(self, thread['function'], None)
+                        if function:
+                            try:
+                                await function()
+                            except Exception as e:
+                                log(f'MiniGame {self.__qualname__} ERROR in thread: {e}', 3)
+
+                            thread['last_start'] = now
+
+                            if thread['col_repeat'] != 'inf':
+                                thread['col_repeat'] -= 1
+
+                                if thread['col_repeat'] <= 0:
+                                    del self.active_threads[key]
+
+                            await self.Update()
+
+    # ======== THREADS ======== #
     async def timer(self):
         """ Поток работы таймера """
         pass
@@ -64,11 +102,16 @@ class MiniGame:
         self.chat_id = chat_id
         self.user_id = user_id
 
+        # Сохраняем данные в базе
         self.session_key = await SessionGenerator()
         Registry.save_class_object(self.GAME_ID, self.session_key, self)
-        await self.Update()
+        await insert_session(self.__dict__)
 
+        # Отправляем сообщение
         await self.MessageGenerator()
+
+        # Запускаем доп процессы (Последняя строчка в функции)
+        await self.run_threads()
 
     async def ContinueGame(self, code: str) -> None:
         """ Когда игра продолжается после неожиданного завершения"""
@@ -77,12 +120,18 @@ class MiniGame:
 
         await self.MessageGenerator()
 
+        # Запускаем доп процессы (Последняя строчка в функции)
+        await self.run_threads()
+
     # ======== END ======== #
     """ Когда игра завершена """
 
     async def EndGame(self): 
         """ Заканчивает игру """
-        await delete_session(self.session_key)
+        self.__threads_work = False
+        self.active_threads = False
+
+        await delete_session(self._id)
         Registry.delete_class_object(self.GAME_ID, self.session_key)
 
     # ======== MARKUP ======== #
@@ -156,7 +205,7 @@ class MiniGame:
 
     async def Update(self):
         """ Обновление данных в базе """
-        await update_session(self.session_key, self.__dict__)
+        await update_session(self._id, self.__dict__)
 
     def GetButton(self, key: str):
         """ Возвращение функции отвечающей за кнопку """
@@ -181,18 +230,22 @@ async def check_session(session_key: str) -> bool:
                                   comment='check_session_minigame')
     return bool(res)
 
-async def update_session(session_key: str, data: dict) -> None:
-    upd = await database.update_one({'session_key': session_key}, 
+async def update_session(_id: ObjectId, data: dict) -> None:
+    upd = await database.update_one({'_id': _id}, 
                               {'$set': data},
                               comment='update_session_minigame')
-    if upd.modified_count == 0:
-        del data['_id']
-        await database.insert_one(data,
-                                  comment='update_session_minigame')
+    # if upd.modified_count == 0:
+    #     del data['_id']
+    #     await database.insert_one(data,
+    #                               comment='update_session_minigame')
+    
+async def insert_session(data: dict) -> ObjectId:
+    del data['_id']
+    result = await database.insert_one(data, comment='insert_session_minigame')
+    return result.inserted_id
 
-async def delete_session(session_key: str) -> None:
-    await database.delete_one({'session_key': session_key},
-                              comment='delete_session_minigame')
+async def delete_session(_id: ObjectId) -> None:
+    await database.delete_one({'_id': _id}, comment='delete_session_minigame')
 
 async def get_session(session_key: str) -> dict:
     data = await database.find_one({'session_key': session_key},
