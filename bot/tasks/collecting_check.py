5f4f83a60dfa333ba26a03202935f13d681c1f33
AS1 <arseniykofanov@gmail.com>
2025-03-31 20:38:43 +0300

    Новый способ работы сбора пищи (группы и новый рандом)

--- bot/tasks/collecting_check.py
+++ bot/tasks/collecting_check.py
@@ -8,6 +8,8 @@ from bot.modules.data_format import transform
 from bot.modules.items.accessory import check_accessory
 from bot.modules.dinosaur.dinosaur import Dino, end_collecting, mutate_dino_stat
 from bot.modules.items.item import counts_items
+from bot.modules.items.item_tools import rare_random
+from bot.modules.items.items_groups import get_group
 from bot.modules.localization import  get_lang
 from bot.modules.dinosaur.mood import check_inspiration
 from bot.modules.quests import quest_process
@@ -46,7 +48,9 @@ async def collecting_process():
             await stop_collect(coll_data)
         else:
             dino = await Dino().create(coll_data['dino_id'])
-            rare_list = [50, 25, 15, 9, 1]
+
+            special_chance = {}
+            chances_add = {'common': 0, 'uncommon': 0, 'rare': 0, 'mystical': 0, 'legendary': 0}
 
             # Понижение энергии
             if random() <= ENERGY_DOWN:
@@ -79,22 +83,16 @@ async def collecting_process():
                 if coll_type == 'fishing' and \
                     await check_accessory(dino, 'fishing-rod', True):
                         # Аксессуар удочка задействован
-                        rare_list[0] -= 10
-                        rare_list[1] -= 7
-
-                        rare_list[2] += 10
-                        rare_list[3] += 5
-                        rare_list[4] += 2
+                        chances_add['rare'] += 10
+                        chances_add['mystical'] += 5
+                        chances_add['legendary'] += 2
 
                 elif coll_type == 'hunt' and \
                     await check_accessory(dino, 'net', True):
                         # Аксессуар сеть задействован
-                        rare_list[0] -= 10
-                        rare_list[1] -= 7
-
-                        rare_list[2] += 10
-                        rare_list[3] += 5
-                        rare_list[4] += 2
+                        chances_add['rare'] += 10
+                        chances_add['mystical'] += 5
+                        chances_add['legendary'] += 2
 
                 # # ==== Повышение шанса в зависимости от навыка === #
                 if coll_type == 'collecting':
@@ -110,40 +108,49 @@ async def collecting_process():
                     char = dino.stats['charisma']
     
                 # Распределно по принципу distribute_number()
-                rare_list[0] -= transform(char, 20, 25)
-                rare_list[1] -= transform(char, 20, 12)
+                chances_add['rare'] += transform(char, 20, 22)
+                chances_add['mystical'] += transform(char, 20, 13)
+                chances_add['legendary'] += transform(char, 20, 2)
 
-                rare_list[2] += transform(char, 20, 22)
-                rare_list[3] += transform(char, 20, 13)
-                rare_list[4] += transform(char, 20, 2)
+                # Получение предметов по занятию
+                items = {}
+                if coll_type == 'all':
+                    items = []
+                    for i in ['collecting', 'hunt', 'fishing']:
+                        items += get_group(f'{i}-activity')
+                else: 
+                    items = get_group(f'{coll_type}-activity')
 
-                items = GAME_SETTINGS['collecting_items'][coll_type]
-                event = await get_event(f'add_{coll_type}')
+                # Установка количества предметов
                 count = randint(1, 3)
-
                 if coll_data['now_count'] + count > coll_data['max_count']:
                     count = coll_data['max_count'] - coll_data['now_count']
 
-                for _ in range(count):
-                    rar = choices(list(items.keys()), rare_list)[0]
-                    item = choice(items[rar])
+                # Добавление в шанс предметов события
+                event = await get_event(f'add_{coll_type}')
+                if event: 
+                    items += event['data']['items']
 
-                    if coll_type == 'collecting' and \
-                       random() <= 0.7 and \
-                            await check_accessory(dino, 'torch', True):
-                                # 0.7 выдать редкий предмет
-                                item = 'gourmet_herbs'
+                # Добавление в шанс предметов из аксессуара
+                trc_flag = False
+                if coll_type == 'collecting' and await check_accessory(dino, 'torch'):
+                    # Шанс на изысканные травы равен 15% если есть факел
+                    # Иначе шанс от редкости
+                    special_chance['gourmet_herbs'] = 15
+                    trc_flag = True
 
-                    elif event != {} and random() <= 0.2:
-                            # 0.2 Шанс выдать предмет из ивента
-                            item = choice(event['data']['items'])
+                rand_items = rare_random(items, count, chances_add, special_chance)
+
+                for item in rand_items:
+
+                    if trc_flag: await check_accessory(dino, 'torch', True)
 
                     if item in coll_data['items']:
                         coll_data['items'][item] += 1
                     else: coll_data['items'][item] = 1
 
                 await long_activity.update_one({'_id': coll_data['_id']}, 
-                                                 {'$set': {'items': coll_data['items'] },'$inc': {'now_count': count}}, comment='collecting_task_1')
+                                                 {'$set': {'items': coll_data['items'] },'$inc': {'now_count': count}}, comment = 'collecting_task_1')
 
                 if coll_data['now_count'] + count == coll_data['max_count']:
                     await stop_collect(coll_data)
