caaa05a138e885daa93e3e123220a263509aead2
AS1 <arseniykofanov@gmail.com>
2025-04-08 21:13:15 +0300

    Сделана проверка только на ответ боту

--- bot/filters/reply_message.py
+++ bot/filters/reply_message.py
@@ -1,14 +1,26 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
+from bot.exec import bot
+
+
 
 class IsReply(BaseFilter):
     def __init__(self, status: bool = True) -> None:
-        self.reply_status: bool = status
+        self.status: bool = status
 
     async def __call__(self, message: Message) -> bool:
-        reply_status = bool(message.reply_to_message)
+        me = await bot.get_me()
 
-        if self.reply_status: result = reply_status
-        else: result = not reply_status
+        reply_status = bool(message.reply_to_message)
+        if reply_status:
+            to_me = me.id == message.reply_to_message.from_user.id
+        else: to_me = False
+
+        status = all(
+            [reply_status, to_me]
+        )
+
+        if self.status: result = status
+        else: result = not status
         return result
 
