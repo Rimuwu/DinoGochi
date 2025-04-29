2b58629a47a641d244864554851fdce50eb9a55c
AS1 <arseniykofanov@gmail.com>
2025-04-29 21:07:42 +0300

    Перенос функции в модуль

--- bot/exec.py
+++ bot/exec.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher, Router
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from bot.config import conf
-from bot.modules.logs import log
+from bot.modules.logs import log, report_devs_start
 from bot.taskmanager import add_task
 from bot.taskmanager import run as run_taskmanager
 import asyncio
@@ -15,20 +15,6 @@ dp = Dispatcher(storage=STORAGE) #storage=MongoStorage(mongo_client)
 
 main_router = Router(name='MainRouter')
 dp.include_router(main_router)
-
-async def report_devs_start():
-    tasks = []
-    report_id = conf.bot_report_id
-
-    if report_id == 0: return
-
-    if isinstance(report_id, str):
-        channel_id, topic_id = report_id.split('_', 1)
-        tasks.append(bot.send_message(channel_id, '✅ Бот запущен!', message_thread_id=int(topic_id)))
-    else: 
-        tasks.append(bot.send_message(report_id, '✅ Бот запущен!'))
-
-    await asyncio.gather(*tasks)
 
 def run():
     log('# ====== Inicialization Start ====== #', 2)
@@ -46,7 +32,7 @@ def run():
         check()
 
         # Запуск тасков и бота
-        add_task(report_devs_start) # Уведомление запуска для разрабов
+        add_task(report_devs_start, bot=bot) # Уведомление запуска для разрабов
         add_task(dp.start_polling, bots=[bot], 
                  allowed_updates=dp.resolve_used_update_types())
 
