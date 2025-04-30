26708893082a138f6d4f48b9ebb4a8ce001c59bf
AS1 <arseniykofanov@gmail.com>
2025-04-30 21:46:33 +0300

    Улучшение команды профиль для групп

--- bot/handlers/profile_menu/info_user.py
+++ bot/handlers/profile_menu/info_user.py
@@ -1,7 +1,10 @@
 
+from pprint import pprint
 from bot.dbmanager import mongo_client
 from bot.exec import main_router, bot
+from bot.filters.group_filter import GroupRules
 from bot.modules.decorators import  HDMessage
+from bot.modules.groups import add_message
 from bot.modules.localization import  get_lang
 from bot.modules.overwriting.DataCalsses import DBconstructor
 from bot.modules.user.user import user_info
@@ -31,16 +34,21 @@ async def infouser(message: Message):
             await bot.send_message(message.chat.id, text, parse_mode='Markdown')
 
 @HDMessage
-@main_router.message(Command(commands=['profile']), IsAuthorizedUser())
+@main_router.message(Command(commands=['profile']), 
+                     GroupRules(True))
 async def infouser_com(message: Message):
     userid = message.from_user.id
-    chatid = message.chat.id
     lang = await get_lang(message.from_user.id)
-    
+
+    args = message.text.split(' ')[1:]
+
     if message.reply_to_message and message.reply_to_message.from_user:
         user_id = message.reply_to_message.from_user.id
     else:
         user_id = userid
+    
+    if args:
+        if args[0] == 'me': user_id = userid
 
     if message.from_user:
         user_exists = await users.find_one({'userid': user_id}, comment='check_user_exists')
@@ -48,6 +56,25 @@ async def infouser_com(message: Message):
         text, avatar = await user_info(user_id, lang)
 
         if avatar:
-            await bot.send_photo(chatid, avatar, caption=text, parse_mode='Markdown')
+            mes = await message.answer_photo(avatar, caption=text, parse_mode='Markdown')
         else:
-            await bot.send_message(message.chat.id, text, parse_mode='Markdown')
+            mes = await message.answer(text, parse_mode='Markdown')
+
+        await add_message(message.chat.id, message.message_id)
+        await add_message(message.chat.id, mes.message_id)
+
+@main_router.message(Text('help_command.commands.profile.alternative'), GroupRules())
+async def infouser_alt(message: Message):
+    userid = message.from_user.id
+    chatid = message.chat.id
+    lang = await get_lang(message.from_user.id)
+
+    text, avatar = await user_info(userid, lang)
+
+    if avatar:
+        mes = await message.answer_photo(avatar, caption=text, parse_mode='Markdown')
+    else:
+        mes = await message.answer(text, parse_mode='Markdown')
+    
+    await add_message(message.chat.id, message.message_id)
+    await add_message(message.chat.id, mes.message_id)
