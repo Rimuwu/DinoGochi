34d39fea7036ba364c86054cee579d45f557cc27
AS1 <arseniykofanov@gmail.com>
2025-05-16 20:16:57 +0300

    вынос функции

---
+++ bot/modules/user/premium.py
@@ -0,0 +1,10 @@
+
+
+from bot.modules.overwriting.DataCalsses import DBconstructor
+from bot.dbmanager import mongo_client
+
+subscriptions = DBconstructor(mongo_client.other.subscriptions)
+
+async def premium(userid: int):
+    res = await subscriptions.find_one({'userid': userid}, comment='premium_res')
+    return bool(res)
