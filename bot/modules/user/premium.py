1415ae05b86027fd99baeb9001cea2c4f58c6e84
AS1 <arseniykofanov@gmail.com>
2025-05-18 14:59:45 +0300

    Исправление пути к подпискам пользователя в модуле premium

--- bot/modules/user/premium.py
+++ bot/modules/user/premium.py
@@ -3,7 +3,7 @@
 from bot.modules.overwriting.DataCalsses import DBconstructor
 from bot.dbmanager import mongo_client
 
-subscriptions = DBconstructor(mongo_client.other.subscriptions)
+subscriptions = DBconstructor(mongo_client.user.subscriptions)
 
 async def premium(userid: int):
     res = await subscriptions.find_one({'userid': userid}, comment='premium_res')
