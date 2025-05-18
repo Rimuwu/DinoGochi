2b1d7092514c9f1772831fce5eb87f8303ffe073
AS1 <arseniykofanov@gmail.com>
2025-05-18 18:08:21 +0300

    Документация и распределение множителя

--- bot/modules/user/xp_boost.py
+++ bot/modules/user/xp_boost.py
@@ -1,21 +1,28 @@
 
-from bot.const import GAME_SETTINGS
 from bot.modules.managment.events import get_event
 from bot.modules.user.premium import premium
 from bot.modules.user.rtl_name import check_name
-from bot.dbmanager import mongo_client
-from bot.exec import bot
 
 from bot.modules.managment.boost_spy import user_boost_channel_status
 
 async def xpboost_percent(userid: int):
+    """
+        Возвращает множитель опыта для пользователя.
+        1.0 - обычный опыт
+        +0.5 - премиум
+        +1.0 - Бустер канала
+        +0.2 - РТЛ имя
+        +0.1 - 0.2 - событие
+
+        Максимум *2.5
+    """
     xp_boost = 1
 
     if await user_boost_channel_status(userid):
-        xp_boost += 0.5
+        xp_boost += 1.0
 
     if await premium(userid):
-        xp_boost += 0.8
+        xp_boost += 0.5
 
     if event_data := await get_event('xp_boost'):
         xp_boost += event_data['data']['xp_boost']
@@ -28,4 +35,4 @@ async def xpboost_percent(userid: int):
 
     # Бустер опыта
 
-    return xp_boost
+    return max(xp_boost, 2.5)
