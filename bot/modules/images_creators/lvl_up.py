23831affca2a57244e712d7f2799a7ac40a2c98c
AS1 <arseniykofanov@gmail.com>
2025-05-18 18:09:03 +0300

    Фикс

--- bot/modules/images_creators/lvl_up.py
+++ bot/modules/images_creators/lvl_up.py
@@ -10,6 +10,9 @@ from bot.exec import bot
 from bot.modules.data_format import pil_image_to_file
 from bot.modules.images import async_open, trans_paste
 
+from aiogram.types import BufferedInputFile
+from bot.exec import bot
+
 back_file = 'images/lvl_up/bg.png'
 upper_file = 'images/lvl_up/upper.png'
 
@@ -27,7 +30,7 @@ def crop_circle(img: Image.Image, size: int) -> Image.Image:
     result.paste(img, (0, 0), mask)
     return result
 
-async def lvl_up_image(avatar_file: str | Any = ''):
+async def lvl_up_image(avatar_file: str | BufferedInputFile = ''):
     # Открываем фоновое изображение
     img = Image.open(back_file).convert("RGBA")
 
@@ -42,7 +45,7 @@ async def lvl_up_image(avatar_file: str | Any = ''):
             avatar = await async_open('images/remain/dinogochi_user.png')
     else:
         # Если avatar_file — это BufferedInputFile, читаем его содержимое и открываем как изображение
-        imageStream = io.BytesIO(await avatar_file.read())
+        imageStream = io.BytesIO(avatar_file.data)
         avatar = Image.open(imageStream).convert('RGBA')
 
     # Открываем и обрезаем аватар
