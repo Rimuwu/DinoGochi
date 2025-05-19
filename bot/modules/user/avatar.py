105c051128f5d8fa520cfb6495a2216fd7732499
AS1 <arseniykofanov@gmail.com>
2025-05-19 19:02:17 +0300

    Фикс

--- bot/modules/user/avatar.py
+++ bot/modules/user/avatar.py
@@ -1,4 +1,5 @@
 
+from io import BufferedReader
 from bot.exec import bot
 from bot.modules.images import async_open
 from bot.dbmanager import mongo_client
@@ -7,27 +8,28 @@ from bot.modules.overwriting.DataCalsses import DBconstructor
 users = DBconstructor(mongo_client.user.users)
 
 async def get_avatar(user_id: int):
-    """Возвращает аватарку пользователя, если файл устарел - обновляет и возвращает новую."""
+    """Возвращает file_id аватара пользователя или файл, если file_id устарел. Если аватара нет — возвращает дефолт."""
     user = await users.find_one({'userid': user_id}, comment='get_avatar') or {}
 
-    if not user.get('avatar'):
-        # Если аватар не установлен, пробуем получить новый
-        photos = await bot.get_user_profile_photos(user.get('userid', 0), limit=1)
-        if photos.photos:
-            photo_id = photos.photos[0][0].file_id
-            user['avatar'] = photo_id
-            await users.update_one({'userid': user_id}, {'$set': {'avatar': photo_id}})
-        else:
-            avatar = await async_open('images/remain/dinogochi_user.png', True)
+    avatar_id = user.get('avatar')
+    if avatar_id:
+        try:
+            # Проверяем, доступен ли файл по file_id
+            await bot.get_file(avatar_id)
+            return avatar_id
+        except Exception:
+            # file_id устарел, сбрасываем
             await users.update_one({'userid': user_id}, {'$set': {'avatar': ''}})
+            avatar: BufferedReader = await async_open('images/remain/dinogochi_user.png', True)  # type: ignore
             return avatar
 
-    try:
-        await bot.get_file(user['avatar'])
-    except Exception:
-        # Если файл не найден или устарел, сбрасываем аватар и возвращаем дефолт
-        avatar = await async_open('images/remain/dinogochi_user.png', True)
+    # Если аватар не установлен или file_id сброшен
+    photos = await bot.get_user_profile_photos(user_id, limit=1)
+    if photos.photos:
+        photo_id = photos.photos[0][0].file_id
+        await users.update_one({'userid': user_id}, {'$set': {'avatar': photo_id}})
+        return photo_id
+    else:
+        avatar: BufferedReader = await async_open('images/remain/dinogochi_user.png', True)  # type: ignore
         await users.update_one({'userid': user_id}, {'$set': {'avatar': ''}})
         return avatar
-
-    return user['avatar']
