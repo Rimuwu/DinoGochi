54165d8aeacaf3b46d5696501555ed3bb5322b41
AS1 <arseniykofanov@gmail.com>
2025-07-08 21:11:51 +0300

    получение безопасных клеток

--- bot/modules/map/map_data.py
+++ bot/modules/map/map_data.py
@@ -1,12 +1,12 @@
 
 import string
-from typing import Any
+from typing import Any, Union
 from bot.const import MAP
 
 def get_island_cells(island_name: str) -> list[tuple[int, int]]:
     """
     Получает список координат клеток острова в виде кортежей (x, y).
-    
+
     :param island_name: Название острова.
     :return: Список кортежей с координатами клеток (x, y).
     """
@@ -22,7 +22,7 @@ def get_island_cells(island_name: str) -> list[tuple[int, int]]:
 def get_island_cells_letter(island_name: str) -> list[str]:
     """
     Получает список координат клеток острова в виде строк (например, 'A6').
-    
+
     :param island_name: Название острова.
     :return: Список строк с координатами клеток.
     """
@@ -47,9 +47,18 @@ def letter_to_number(letter: str):
 
 def string_cell_to_list(cell_str):
     """
-    Преобразует строку вида '1,2' в список [1, 2].
+    Преобразует строку вида '1.2' в список [1, 2].
     """
     return [int(x) for x in cell_str.split('.')]
+
+def cell_to_string(cell: Union[tuple[int, int], list[int]]) -> str:
+    """
+    Преобразует координаты клетки в строку формата 'x.y'.
+    
+    :param cell: Кортеж или список с координатами клетки (x, y).
+    :return: Строка с координатами клетки.
+    """
+    return f"{cell[0]}.{cell[1]}"
 
 def get_island_image(island_name: str) -> str:
     """
@@ -84,7 +93,7 @@ async def get_wagon_for_island(island_name: str):
     :return: Кортеж с координатами где находится вагон и список ячеек с путями.
     """
 
-    wagon_cords: tuple[int, int] | None = ()
+    wagon_cords: list[int] | None = []
     wey_cells: list[tuple[int, int]] = []
 
     # Тут код получения из бд
@@ -160,3 +169,26 @@ async def data_object_on_map(island_name: str,
             })
 
     return island_objects, islands_ways
+
+def get_safe_cells(island: str) -> list[list[str]]:
+    """
+    Получает список безопасных ячеек для указанного острова.
+    :param island: Название острова.
+    :return: Список безопасных ячеек в формате x.y
+    """
+    safe_cells = []
+
+    for cell in MAP.get(island, {}).get('safe_cells', []):
+        cell: dict[str, Any] = cell
+
+        safe_cells.append(cell['cell'])
+        if cell['radius'] > 1:
+            it_cell = string_cell_to_list(cell['cell'])
+            
+            for dx in range(-cell['radius'], cell['radius'] + 1):
+                for dy in range(-cell['radius'], cell['radius'] + 1):
+                    new_cell = f"{it_cell[0] + dx}.{it_cell[1] + dy}"
+                    if new_cell not in safe_cells:
+                        safe_cells.append(new_cell)
+
+    return safe_cells
